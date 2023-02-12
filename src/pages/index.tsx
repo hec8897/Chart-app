14ee1dc4248d958de0da71ead1286caae0b4a914
dawoonkim <hec8897@naver.com>
2023-02-12 12:55:51 +0900

    tailwind install
    Signed-off-by: dawoonkim <hec8897@naver.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { PageContainer } from "component";
 const Home = () => {
   return (
     <PageContainer>
-      <div>홈</div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </PageContainer>
   );
 };
