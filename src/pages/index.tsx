258bfe76e0d05b223005761d870da82808e8fe7e
dawoonkim <hec8897@naver.com>
2023-03-02 23:59:19 +0900

    component apply
    Signed-off-by: dawoonkim <hec8897@naver.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,21 @@ import { PageContainer } from "component";
 import { useRequest } from "hooks";
 import { useQuery } from "react-query";
 
+const Title = ({ title }: { title: string }) => {
+  return <div>{title}</div>;
+};
+
 const Home = () => {
   const { getList } = useRequest();
   const { data } = useQuery("list", getList, {
     suspense: true,
-    onSuccess(data) {
-      console.log(data);
-    },
   });
   return (
     <PageContainer>
-      <Suspense fallback={<div>loading</div>}>
-        <div>{data.title}</div>
+      <Suspense
+        fallback={<div>loadingloadingloadingloadingloadingloading</div>}
+      >
+        <Title title={data.title} />
       </Suspense>
     </PageContainer>
   );
