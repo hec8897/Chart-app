3950af4b5e1841c2158b1576256081afbeffe342
김다운 <37240562+hec8897@users.noreply.github.com>
2023-03-05 23:05:47 +0900

    Auto login (#6)
    * auto login amend
    
    Signed-off-by: dawoonkim <hec8897@naver.com>
    
    * auto login
    
    Signed-off-by: dawoonkim <hec8897@naver.com>
    
    ---------
    
    Signed-off-by: dawoonkim <hec8897@naver.com>

--- src/component/HOC/Autho.tsx
+++ src/component/HOC/Autho.tsx
@@ -2,23 +2,23 @@ import React, { useEffect, ReactNode } from "react";
 import { useRouter } from "next/router";
 import { useStorage } from "hooks";
 import { authoState } from "store/autho";
-import { useSetRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 
 const Autho = ({ children }: { children: ReactNode }) => {
   const { getSession } = useStorage();
   const { push, replace } = useRouter();
-  const setValue = useSetRecoilState(authoState);
+  const [value, setValue] = useRecoilState(authoState);
 
   useEffect(() => {
     const autho: { name: string; uid: string } = getSession("autho");
     if (autho) {
-      setValue({ name: autho.name, uid: autho.uid });
+      setValue({ name: autho.name, uid: autho.uid, login: true });
       push("/");
     } else {
       replace("/signin");
     }
   }, []);
 
-  return <>{children}</>;
+  return <>{value.login && children}</>;
 };
 export default Autho;
