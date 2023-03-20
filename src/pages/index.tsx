ccf67a3e7dabc65c01cd7949546e398b1ab6b88a
김다운 <37240562+hec8897@users.noreply.github.com>
2023-03-20 22:02:39 +0900

    selector (#7)
    Signed-off-by: dawoonkim <hec8897@naver.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,15 @@ import { PageContainer } from "component";
 import { useRequest } from "hooks";
 import { useQuery } from "react-query";
 import { Autho } from "component";
+import { listSelector } from "store/list";
+import { useRecoilValue } from "recoil";
 
 const Title = ({ title }: { title: string }) => {
   return <div>{title}</div>;
 };
 
 const Home = () => {
+  const { list } = useRecoilValue(listSelector);
   const { getList } = useRequest();
 
   const { data } = useQuery("list", getList, {
@@ -19,6 +22,14 @@ const Home = () => {
     <Autho>
       <PageContainer>
         <Title title={data.title} />
+        {list.map((ele) => {
+          return (
+            <div key={ele.id}>
+              <h2>{ele.name}</h2>
+              <h2>{ele.desc}</h2>
+            </div>
+          );
+        })}
       </PageContainer>
     </Autho>
   );
