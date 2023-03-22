42e7be2bb642200d2dd1a4746fa476464e85db93
dawoonkim <hec8897@naver.com>
2023-03-22 09:34:55 +0900

    atom family mapping
    Signed-off-by: dawoonkim <hec8897@naver.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,38 +4,36 @@ import { useRequest } from "hooks";
 import { useQuery } from "react-query";
 import { Autho } from "component";
 import { familyAtomFamily } from "store/family";
-import { useRecoilValue, useRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 
 const Title = ({ title }: { title: string }) => {
   return <div>{title}</div>;
 };
 
-const BoxComponent = ({
-  data,
-  onClick,
-}: {
-  data: { title: string; count: number };
-  onClick?: () => void;
-}) => (
-  <div className="border p-2 rounded text-center">
-    <h2 className="mb-2 text-xl font-bold">{data?.title}</h2>
-    <p>data:{data?.count}</p>
-    <button className="bg-blue-200 w-full rounded" onClick={onClick}>
-      update
-    </button>
-  </div>
-);
+const BoxComponent = ({ id }: { id: number }) => {
+  const [value, setCount] = useRecoilState(familyAtomFamily(id));
+
+  return (
+    <div className="border p-2 rounded text-center">
+      <h2 className="mb-2 text-xl font-bold">{value?.title}</h2>
+      <p>data:{value?.count}</p>
+      <button
+        className="bg-blue-200 w-full rounded"
+        onClick={() => setCount({ ...value, count: value.count + 1 })}
+      >
+        update
+      </button>
+    </div>
+  );
+};
 
 const Home = () => {
-  const [card1, setCard1] = useRecoilState(familyAtomFamily(0));
-  const card2 = useRecoilValue(familyAtomFamily(1));
+  const familyKey = [1, 2, 3];
   const { getList } = useRequest();
 
   const { data } = useQuery("list", getList, {
     suspense: true,
   });
-
-  console.log(card1);
 
   return (
     <Autho>
@@ -43,11 +41,9 @@ const Home = () => {
         <div className="p-2">
           <Title title={data.title} />
           <div className="flex flex-col gap-2 my-2">
-            <BoxComponent
-              data={card1}
-              onClick={() => setCard1({ ...card1, count: card1.count + 1 })}
-            />
-            <BoxComponent data={card2} />
+            {familyKey.map((ele) => (
+              <BoxComponent key={ele} id={ele} />
+            ))}
           </div>
         </div>
       </PageContainer>
